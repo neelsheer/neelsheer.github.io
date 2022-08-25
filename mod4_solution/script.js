c8dd177c2990b39cee3c871ece83438c2251010b
neelsheer <109023301+neelsheer@users.noreply.github.com>
2022-08-25 08:52:00 +0530

    update

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -1,20 +1,21 @@
 var names=new Array();
 names[0]="Jennifer";
-names[1]="Rahul";
-names[2]="nishi";
-names[3]="Jocky";
+names[1]="nishi";
+names[2]="Rahul";
+names[3]="jocky";
 names[4]="yesh";
-names[5]="jack";
+names[5]="Jack";
 names[6]="vicky";
 names[7]="Jolly";
-names[8]="Lio";
-names[9]="Jelly";
+names[8]="Jio";
+names[9]="Lio";
+names[10]="lata";
 
 for (var i = 0; i<names.length; i++){
-    if(names[i].charAt(0)==='J'||names[i].charAt(0)==='j'){
-        console.log("Goodbye "+ names[i])
-    }
-      else{
-        console.log("Hello " + names[i])
-      }
+  if(names[i].charAt(0)==='J' || names[i].charAt(0)==='j'){
+    console.log("Goodbye "+names[i]);
+  }
+  else{
+    console.log("Goodbye "+names[i]);
+  }
 }
